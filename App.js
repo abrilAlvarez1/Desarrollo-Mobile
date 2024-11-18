a54c8de574bec2d6d32f190ecc46f122dcf23e0d
Abril <abrilalvarez1205@gmail.com>
2024-11-18 15:41:14 -0300

    primer commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return(
+    <View style={styles.container}>
+    <Text>Hols mundo</Text>
+  </View>
+  )
+
+}
+
+
+export default App
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: "red"
+  }
+})
